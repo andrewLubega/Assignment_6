ace5642844bd0ca0ee1e5136ba5dae708b24d847
AndrewLubega <andrew.lubega@hgs.hiddengeniusproject.org>
2024-04-27 20:46:51 -0700

    outbedded style to style.css

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     submitButton.addEventListener('click', function() {
         const text = inputText.value.trim();
         if (text !== '') {
-            // Do something with the submitted text
             console.log('Submitted text:', text);
             alert('Submitted text:\n\n' + text);
-            // You can replace the console.log and alert with your desired functionality
         } else {
             alert('Please enter some text before submitting.');
         }
